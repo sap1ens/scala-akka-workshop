806c06abdd0fa0ba99762519efca18e56b61e6d6
Yaroslav Tkachenko <sapiensy@gmail.com>
2017-02-04 18:00:27 -0800

    Implementation

--- src/main/scala/com/sap1ens/workshop/JobQueueApplication.scala
+++ src/main/scala/com/sap1ens/workshop/JobQueueApplication.scala
@@ -1,5 +1,16 @@
 package com.sap1ens.workshop
 
+import akka.actor.ActorSystem
+import com.sap1ens.workshop.JobExecutor.DownloadRequest
+
 object JobQueueApplication extends App {
-  println("Hey, there! Let's build job queue")
+  implicit val system = ActorSystem("job-queue")
+  
+  val jobExecutor = system.actorOf(JobExecutor.props())
+
+  jobExecutor ! DownloadRequest("https://www.google.com/robots.txt", "/tmp/robots1.txt")
+  jobExecutor ! DownloadRequest("https://www.google.com/robots.txt", "/tmp/robots2.txt")
+  jobExecutor ! DownloadRequest("https://www.google.com/robots.txt", "/tmp/robots3.txt")
+  jobExecutor ! DownloadRequest("https://www.google.com/robots.txt", "/tmp/robots4.txt")
+  jobExecutor ! DownloadRequest("https://www.google.com/robots.txt", "/tmp/robots5.txt")
 }
